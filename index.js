c303505eb0ebfa8813ca43071b1b1196ecdf47ba
konkyr2001 <konkyr2001@hotmail.com>
2023-12-24 10:23:20 +0200

    part3 testing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+const Note = require('./models/note')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
@@ -31,43 +35,84 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.send(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    console.log(note)
-    if (note) {
+app.get('/api/notes/:id', (request, response, next) => {
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+
+    .catch(error => next(error))
+})
+
+
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
 
-  app.post('/api/notes', (request, response) => {
-    const note = request.body;
-  
-    // Add the new note to the notes array using concat
-    notes = notes.concat(note);
-  
-    // Log the updated notes array (optional)
-    console.log(notes);
-  
-    // No response sent back to the client
-    response.send(note)
+    .catch(error => next(error))
+})
 
-  });
-  
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log("Error in delete endpoint:", error); // Log the error details
+      next(error)
+    })
+})
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+  console.log(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
